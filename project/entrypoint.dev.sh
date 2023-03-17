8db27e21f81249af70800f787a451bd83c8c7065
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-17 22:51:45 +0300

    Add prod and dev dockerfiles and entrypoints for them

---
+++ project/entrypoint.dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Waiting for database..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+
+echo "Database started"
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
---
+++ project/entrypoint.prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Waiting for database..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+
+echo "Database started"
+
+# python ../manage.py migrate
+# python ../manage.py collectstatic
+
+exec "$@"
+
