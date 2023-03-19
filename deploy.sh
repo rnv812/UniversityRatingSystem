aa54c4d9fbd4e02a3c70ade8b714eadd4ce0a15d
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-19 10:08:20 +0300

    Add option to specify compose options when running deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [[ $1 = "prod"  || $1 = "dev"]] && [[ $2 = "down" || $2 = "up" ]]; then
+if [[ $1 = "prod" || $1 = "dev"] && [$2 = "down" || $2 = "up" ]]; then
     cd ..
-    fileEnv="docker-compose.${1}.yml"
+    fileEnv="docker-compose.${1}.yaml"
     action=$2
-    echo "Running docker compose -f docker-compose.yaml -f $fileEnv $action"
-    docker compose -f docker-compose.yaml -f $fileEnv $action
+    flags=${@:2}
+    echo "Running docker compose -f $fileEnv $action $flags"
+    docker compose -f $fileEnv $action $flags
 else
-    echo "Use following format ./deploy.sh prod|dev down|up"
+    echo "Use following format ./deploy.sh prod|dev down|up [compose opts]"
 fi
