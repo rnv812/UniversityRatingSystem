b3a9e5e22375e1f7f2850ff622a70ef778c2b0bf
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-20 20:26:10 +0300

    Update messages for deploy and shell-web scripts

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [[ $1 = "prod" || $1 = "dev" ]]; then
     echo "Running docker compose -f $fileEnv up $flags"
     docker compose -f $fileEnv up $flags
 else
-    echo "Use following format ./deploy.sh prod|dev [options]"
+    echo "Use following format: deploy.sh prod|dev [options]"
     echo
     echo "Possible options at https://docs.docker.com/engine/reference/commandline/compose_up/#options"
 fi
