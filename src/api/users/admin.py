b2c70a53517ecd80fcf4ef1baa57d1126579bb66
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-20 21:56:21 +0300

    Add patronomyc to admin panel

--- src/api/users/admin.py
+++ src/api/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from . import forms
 from . import models
@@ -12,3 +13,32 @@ class CustomUserAdmin(UserAdmin):
     model = models.CustomUser
 
     list_display = ('username', 'last_name', 'first_name', 'patronymic', )
+    search_fields = ('username', 'last_name', 'first_name', 'patronymic', 'email')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('last_name', 'first_name', 'patronymic', 'email')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('username', 'last_name', 'first_name', 'patronymic', 'password1', 'password2'),
+            },
+        ),
+    )
