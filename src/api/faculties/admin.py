fcde8b294d629221fe7a150f196c85c089caa022
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-26 21:49:39 +0300

    Add admin class for faculties(#20), update verbose names for faculty model (#22)

--- src/api/faculties/admin.py
+++ src/api/faculties/admin.py
@@ -7,5 +7,5 @@ from .models import Faculty
 @admin.register(Faculty)
 class FacultyAdmin(admin.ModelAdmin):
     list_display = ('name', 'head', )
-    search_fields = ('name', 'head__last_name', )
-    search_help_text = _('Faculty name or head last name')
+    search_fields = ('name', 'head__first_name', 'head__last_name', 'head__patronymic', )
+    search_help_text = _('Faculty name or head first name, last name or patronymic')
