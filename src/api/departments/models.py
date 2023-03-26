6845de1bbfff052690f8c8082dc4b142477d695a
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-26 21:59:56 +0300

    Add admin class (#20) and update verbose names for educator and qualification models (#22)

--- src/api/departments/models.py
+++ src/api/departments/models.py
@@ -8,21 +8,27 @@ from api.users.models import CustomUser
 class DepartmentType(models.Model):
     """Model that represents type of university department."""
 
-    name = models.CharField(verbose_name=_('name'), max_length=100)
+    name = models.CharField(verbose_name=_('department type name'), max_length=100, unique=True)
 
     class Meta:
         verbose_name = _('department type')
         verbose_name_plural = _('department types')
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Department(models.Model):
     """Model that represents university department."""
 
-    name = models.CharField(verbose_name=_('name'), max_length=255)
+    name = models.CharField(verbose_name=_('department name'), max_length=255, unique=True)
     department_type = models.ForeignKey(verbose_name=_('department type'), to=DepartmentType, on_delete=models.PROTECT)
-    head = models.OneToOneField(verbose_name=_('head'), to=CustomUser, on_delete=models.PROTECT)
-    faculty = models.ForeignKey(verbose_name=_('faculty'), to=Faculty, on_delete=models.PROTECT)
+    head = models.OneToOneField(verbose_name=_('department head'), to=CustomUser, on_delete=models.PROTECT)
+    faculty = models.ForeignKey(verbose_name=_('department faculty'), to=Faculty, on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = _('department')
         verbose_name_plural = _('departments')
+
+    def __str__(self) -> str:
+        return self.name
