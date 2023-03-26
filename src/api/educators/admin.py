1c305e7698569ddbcd747038458e5f2d9ec22125
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-26 22:15:55 +0300

    Add admin classes for educators app (#20) and update verbose names for models (#22)

--- src/api/educators/admin.py
+++ src/api/educators/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Educator, Qualification
+
+
+@admin.register(Educator)
+class EducatorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'qualification', 'department', )
+    search_fields = ('user__first_name', 'user__last_name', 'user__patronymic', )
+
+
+@admin.register(Qualification)
+class QualificationAdmin(admin.ModelAdmin):
+    list_display = ('name', )
