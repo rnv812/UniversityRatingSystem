29f908185cfe62f07c00e3341504d9f79b316d56
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-26 18:34:24 +0300

    Add admin class for faculties (#20)

--- src/api/faculties/admin.py
+++ src/api/faculties/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Faculty
+
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'head', )
+    search_fields = ('name', 'head__last_name', )
+    search_help_text = _('Faculty name or head last name')
--- src/api/users/models.py
+++ src/api/users/models.py
@@ -8,6 +8,9 @@ class CustomUser(AbstractUser):
 
     patronymic = models.CharField(verbose_name=_('patronomyc'), max_length=150, blank=True)
 
+    def __str__(self) -> str:
+        return self.get_full_name()
+
     def get_full_name(self) -> str:
         """Return the last_name plus the first_name plus the patronymic, with a space in between."""
         full_name = "%s %s %s" % (self.last_name, self.first_name, self.patronymic)
