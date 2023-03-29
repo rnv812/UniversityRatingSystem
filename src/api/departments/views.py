e2c9b6d80f51beb4ae8adfb4d9b427d6b91ac192
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-03-29 14:12:08 +0300

    Delete unneeded imports form departments views

--- src/api/departments/views.py
+++ src/api/departments/views.py
@@ -1,8 +1,5 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.permissions import SAFE_METHODS, AllowAny
-from rest_framework.decorators import action
-from rest_framework.request import Request
-from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from .models import Department, DepartmentType
 from .serializers import DepartmentSerializer, DepartmentTypeSerializer
