aacf13f0f2a05dd03213f6164d538e0bf30c98d6
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-01 22:58:30 +0300

    Rename educators_rating to educator_rating (#23)

--- src/api/educator_rating/apps.py
+++ src/api/educator_rating/apps.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class EducatorsRatingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api.educators_rating'
-    verbose_name = _('educators rating')
+    name = 'api.educator_rating'
+    verbose_name = _('educator rating')
