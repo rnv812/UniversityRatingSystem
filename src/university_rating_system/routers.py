62716cb5ffcb1b97c5dc1b169ed38baf2d679df7
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-02 20:55:53 +0300

    Move routers to separate file

---
+++ src/university_rating_system/routers.py
@@ -0,0 +1,88 @@
+from rest_framework import routers
+
+from api.users.views import UserViewSet
+from api.faculties.views import FacultyViewSet
+from api.departments.views import DepartmentViewSet, DepartmentTypeViewSet
+from api.educators.views import EducatorViewSet, QualificationViewSet
+from api.rating.views import (ValueTypeViewSet,
+                              RatingPartitionViewSet,
+                              IndicatorViewSet,
+                              CriterionViewSet)
+from api.educator_rating.views import (EducatorRatingPartitionViewSet,
+                                       EducatorIndicatorValueViewSet,
+                                       EducatorReportViewSet,
+                                       EducatorReportControllerViewSet)
+
+
+api_router = routers.DefaultRouter()
+
+api_router.register(
+    prefix='users',
+    viewset=UserViewSet,
+    basename='user'
+)
+api_router.register(
+    prefix='faculties',
+    viewset=FacultyViewSet,
+    basename='faculty'
+)
+api_router.register(
+    prefix='departments',
+    viewset=DepartmentViewSet,
+    basename='department'
+)
+api_router.register(
+    prefix='department-types',
+    viewset=DepartmentTypeViewSet,
+    basename='department-type'
+)
+api_router.register(
+    prefix='educators',
+    viewset=EducatorViewSet,
+    basename='educator'
+)
+api_router.register(
+    prefix='qualifications',
+    viewset=QualificationViewSet,
+    basename='qualification'
+)
+api_router.register(
+    prefix='value-types',
+    viewset=ValueTypeViewSet,
+    basename='value-type'
+)
+api_router.register(
+    prefix='rating-partitions',
+    viewset=RatingPartitionViewSet,
+    basename='rating-partition'
+)
+api_router.register(
+    prefix='indicators',
+    viewset=IndicatorViewSet,
+    basename='indicator'
+)
+api_router.register(
+    prefix='criterions',
+    viewset=CriterionViewSet,
+    basename='criterion'
+)
+api_router.register(
+    prefix='educator-partitions',
+    viewset=EducatorRatingPartitionViewSet,
+    basename='educator-partition'
+)
+api_router.register(
+    prefix='educator-indicator-values',
+    viewset=EducatorIndicatorValueViewSet,
+    basename='educator-indicator-value'
+)
+api_router.register(
+    prefix='educator-reports',
+    viewset=EducatorReportViewSet,
+    basename='educator-report'
+)
+api_router.register(
+    prefix='educator-report-controllers',
+    viewset=EducatorReportControllerViewSet,
+    basename='educator-report-controller'
+)
--- src/university_rating_system/urls.py
+++ src/university_rating_system/urls.py
@@ -19,94 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.views.generic import TemplateView
 
-from rest_framework import routers
 from rest_framework.schemas import get_schema_view
 
-from api.users.views import UserViewSet
-from api.faculties.views import FacultyViewSet
-from api.departments.views import DepartmentViewSet, DepartmentTypeViewSet
-from api.educators.views import EducatorViewSet, QualificationViewSet
-from api.rating.views import (ValueTypeViewSet,
-                              RatingPartitionViewSet,
-                              IndicatorViewSet,
-                              CriterionViewSet)
-from api.educator_rating.views import (EducatorRatingPartitionViewSet,
-                                       EducatorIndicatorValueViewSet,
-                                       EducatorReportViewSet,
-                                       EducatorReportControllerViewSet)
-
-
-api_router = routers.DefaultRouter()
-api_router.register(
-    prefix='users',
-    viewset=UserViewSet,
-    basename='user'
-)
-api_router.register(
-    prefix='faculties',
-    viewset=FacultyViewSet,
-    basename='faculty'
-)
-api_router.register(
-    prefix='departments',
-    viewset=DepartmentViewSet,
-    basename='department'
-)
-api_router.register(
-    prefix='department-types',
-    viewset=DepartmentTypeViewSet,
-    basename='department-type'
-)
-api_router.register(
-    prefix='educators',
-    viewset=EducatorViewSet,
-    basename='educator'
-)
-api_router.register(
-    prefix='qualifications',
-    viewset=QualificationViewSet,
-    basename='qualification'
-)
-api_router.register(
-    prefix='value-types',
-    viewset=ValueTypeViewSet,
-    basename='value-type'
-)
-api_router.register(
-    prefix='rating-partitions',
-    viewset=RatingPartitionViewSet,
-    basename='rating-partition'
-)
-api_router.register(
-    prefix='indicators',
-    viewset=IndicatorViewSet,
-    basename='indicator'
-)
-api_router.register(
-    prefix='criterions',
-    viewset=CriterionViewSet,
-    basename='criterion'
-)
-api_router.register(
-    prefix='educator-partitions',
-    viewset=EducatorRatingPartitionViewSet,
-    basename='educator-partition'
-)
-api_router.register(
-    prefix='educator-indicator-values',
-    viewset=EducatorIndicatorValueViewSet,
-    basename='educator-indicator-value'
-)
-api_router.register(
-    prefix='educator-reports',
-    viewset=EducatorReportViewSet,
-    basename='educator-report'
-)
-api_router.register(
-    prefix='educator-report-controllers',
-    viewset=EducatorReportControllerViewSet,
-    basename='educator-report-controller'
-)
+from .routers import api_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
