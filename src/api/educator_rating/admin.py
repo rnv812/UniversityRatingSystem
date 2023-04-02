ef32e75fc9686c7d5a5239bba1f95f2194d422f9
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-02 16:09:47 +0300

    Register EducatorReportController in admin site (#33)

--- src/api/educator_rating/admin.py
+++ src/api/educator_rating/admin.py
@@ -3,7 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 from .models import (EducatorRatingPartition,
                      EducatorIndicatorValue,
-                     EducatorReport)
+                     EducatorReport,
+                     EducatorReportController)
 
 
 @admin.register(EducatorRatingPartition)
@@ -26,3 +27,15 @@ class EducatorReportAdmin(admin.ModelAdmin):
     list_select_related = ('educator__user', 'educator__qualification')
     search_help_text = _('Educator first name, lastname or patronymic')
     list_filter = ('educator__qualification__name', 'year', )
+
+
+@admin.register(EducatorReportController)
+class EducatorReportControllerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'department', )
+    search_fields = (
+        'user__username', 'user__first_name',
+        'user__last_name', 'user__patronymic',
+    )
+    search_help_text = _(
+        'Controller username, first name, last name or patronymic'
+    )
