29d354a10b0aeee86a3a558c3f71b92aae5e06de
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-03 00:24:10 +0300

    Change api url to root (#30)

--- src/university_rating_system/urls.py
+++ src/university_rating_system/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.core.urls')),
+    path('', include('api.core.urls')),
 ]
