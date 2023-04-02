97f31ffcbe674008f9fc73a8f5fbaa7315e43006
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-03 00:42:46 +0300

    Add admin url changing in production mode

--- src/university_rating_system/urls.py
+++ src/university_rating_system/urls.py
@@ -1,8 +1,21 @@
+import os
+from uuid import uuid4
+
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('api.core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('admin/', admin.site.urls))
+else:
+    urlpatterns.append(
+        path(
+            f'{os.getenv("ADMIN_SITE_URL", str(uuid4()))}/',
+            admin.site.urls
+        )
+    )
