8232b5031fa914df872b27ec69e544eeefe8e345
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-03 15:28:05 +0300

    Add search fields to TokenAdmin

---
+++ src/api/core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.authtoken.models import TokenProxy
+from rest_framework.authtoken.admin import TokenAdmin
+
+
+admin.site.unregister(TokenProxy)
+
+
+# https://github.com/encode/django-rest-framework/discussions/8927
+@admin.register(TokenProxy)
+class TokenCustomAdmin(TokenAdmin):
+    search_fields = ('user__username', )
+    search_help_text = _('Username')
