6e6f5c39866537eb03d816bf9d42f772ce514246
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-05 15:00:39 +0300

    Update permissions for educators and rating app viewsets

--- src/api/educators/views.py
+++ src/api/educators/views.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.permissions import AllowAny
 
 from .models import Educator, Qualification
 from .serializers import EducatorSerializer, QualificationSerializer
@@ -8,10 +7,8 @@ from .serializers import EducatorSerializer, QualificationSerializer
 class EducatorViewSet(ReadOnlyModelViewSet):
     queryset = Educator.objects.all().order_by('pk')
     serializer_class = EducatorSerializer
-    permission_classes = (AllowAny, )
 
 
 class QualificationViewSet(ReadOnlyModelViewSet):
     queryset = Qualification.objects.all().order_by('pk')
     serializer_class = QualificationSerializer
-    permission_classes = (AllowAny, )
--- src/api/rating/views.py
+++ src/api/rating/views.py
@@ -1,32 +1,29 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.permissions import AllowAny
 
-from .models import ValueType, RatingPartition, Indicator, Criterion
-from .serializers import (ValueTypeSerializer,
-                          RatingPartitionSerializer,
-                          IndicatorSerializer,
-                          CriterionSerializer)
+from .models import Criterion, Indicator, RatingPartition, ValueType
+from .serializers import (
+    CriterionSerializer,
+    IndicatorSerializer,
+    RatingPartitionSerializer,
+    ValueTypeSerializer,
+)
 
 
 class ValueTypeViewSet(ReadOnlyModelViewSet):
     queryset = ValueType.objects.all().order_by('pk')
     serializer_class = ValueTypeSerializer
-    permission_classes = (AllowAny, )
 
 
 class RatingPartitionViewSet(ReadOnlyModelViewSet):
     queryset = RatingPartition.objects.all().order_by('pk')
     serializer_class = RatingPartitionSerializer
-    permission_classes = (AllowAny, )
 
 
 class IndicatorViewSet(ReadOnlyModelViewSet):
     queryset = Indicator.objects.all().order_by('pk')
     serializer_class = IndicatorSerializer
-    permission_classes = (AllowAny, )
 
 
 class CriterionViewSet(ReadOnlyModelViewSet):
     queryset = Criterion.objects.all().order_by('pk')
     serializer_class = CriterionSerializer
-    permission_classes = (AllowAny, )
