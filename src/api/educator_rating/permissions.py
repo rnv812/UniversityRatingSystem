4dc5ccde7413c2e956fab2058bd497abccb602e6
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-05 19:11:57 +0300

    Add custom permissions for educator_rating viewsets

--- src/api/educator_rating/permissions.py
+++ src/api/educator_rating/permissions.py
@@ -1,10 +1,13 @@
 from django.utils.translation import gettext_lazy as _
 
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.request import Request
 from rest_framework.views import View
 
-from .models import EducatorReportController
+from .models import (
+    EducatorIndicatorValue,
+    EducatorReportController,
+)
 
 
 class IsEducatorReportController(BasePermission):
@@ -16,3 +19,59 @@ class IsEducatorReportController(BasePermission):
             return True
         except EducatorReportController.DoesNotExist:
             return False
+
+
+class IsValueOwner(BasePermission):
+    message = _(
+        "You can operate only values of reports owned by you."
+    )
+
+    def has_object_permission(
+        self,
+        request: Request,
+        view: View,
+        obj: EducatorIndicatorValue
+    ) -> bool:
+        return obj.report.educator.user == request.user
+
+
+class IsValueController(BasePermission):
+    message = _(
+        "You can operate only values of reports controlled by you."
+    )
+
+    def has_object_permission(
+        self,
+        request: Request,
+        view: View,
+        obj: EducatorIndicatorValue
+    ) -> bool:
+        try:
+            controller = EducatorReportController.objects.get(
+                user=request.user
+            )
+        except EducatorReportController.DoesNotExist:
+            return False
+
+        return controller.department == obj.report.educator.department
+
+
+class IsOpenForEditOnPatch(BasePermission):
+    message = _("You cannot edit values of report which is already approved.")
+
+    def has_object_permission(
+            self,
+            request: Request,
+            view: View,
+            obj: EducatorIndicatorValue
+    ) -> bool:
+        if request.method in SAFE_METHODS:
+            return True
+
+        return not obj.report.approved
+
+
+class HasEducatorReportAccess(BasePermission):
+    def has_permission(self, request: Request, view: View) -> bool:
+        print(request.data)
+        return False
