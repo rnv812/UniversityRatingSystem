2d97f94991d20e0e7c4c0bfbbec8fbe99291434f
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-07 17:48:32 +0300

    Change exception type to generic for get_object_or_error

--- src/api/core/shortcuts.py
+++ src/api/core/shortcuts.py
@@ -1,16 +1,16 @@
 from django.shortcuts import get_object_or_404
 from django.http.response import Http404
 
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import APIException
 
 
-def get_object_or_validation_error(
-        exception: ValidationError,
+def get_object_or_error(
+        exception: APIException,
         klass,
         *args,
         **kwargs
 ):
-    """Use get() to return an object, or raise ValidationError instance
+    """Use get() to return an object, or raise APIException instance
     passed as first parameter if the object does not exist. Can be used as
     generic version of get_object_or_404 in context of django rest framework.
     """
