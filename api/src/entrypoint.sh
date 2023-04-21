5402f8bb6b61387801f7ac9a654b61eb2f1e7188
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-21 16:30:37 +0300

    Fix tabsize in entrypoint.sh

--- api/src/entrypoint.sh
+++ api/src/entrypoint.sh
@@ -3,16 +3,16 @@
 echo "Waiting for database..."
 
 while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 1
+    sleep 1
 done
 
 echo "Activation environment..."
 
 if [ -d "../.venv/" ]; then
-  source ../.venv/bin/activate
+    source ../.venv/bin/activate
 else
-  echo "No .venv/ found in the /app directory"
-  exit 1
+    echo "No .venv/ found in the /app directory"
+    exit 1
 fi
 
 echo "Applying database migrations..."
@@ -23,22 +23,22 @@ echo "Creating superuser..."
 python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --no-input
 
 if [ $? = 0 ]; then
-  echo "Superuser '$DJANGO_SUPERUSER_USERNAME' created successfully"
+    echo "Superuser '$DJANGO_SUPERUSER_USERNAME' created successfully"
 else
-  echo "Superuser '$DJANGO_SUPERUSER_USERNAME' already exists"
+    echo "Superuser '$DJANGO_SUPERUSER_USERNAME' already exists"
 fi
 
 echo "Running server ..."
 
 if [ $DEBUG = 0 ]; then
-  echo "Using production mode"
-  echo "Performing security checks..."
-  python manage.py check --deploy
-  echo "Not implemented"
-  exit 0
+    echo "Using production mode"
+    echo "Performing security checks..."
+    python manage.py check --deploy
+    echo "Not implemented"
+    exit 0
 else
-  echo "Using development mode"
-  python manage.py runserver 0.0.0.0:8000
+    echo "Using development mode"
+    python manage.py runserver 0.0.0.0:8000
 fi
 
 echo "Server has started"
