7b70f1d1f97664d6363abd01395f6d2a5ffc745b
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-26 17:43:22 +0300

    Add allowed email model and validation rules  (#48)

--- api/src/apps/users/models.py
+++ api/src/apps/users/models.py
@@ -2,18 +2,46 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from .exceptions import NotAllowedEmailValidationError
+
+
+class AllowedEmail(models.Model):
+    """Model stores emails of users that are allowed to register."""
+
+    email = models.EmailField(verbose_name=_('email address'))
+
+    class Meta:
+        verbose_name = _('allowed email')
+        verbose_name = _('allowed emails')
+
+    def __str__(self) -> str:
+        return f'{self.email}'
+
 
 class CustomUser(AbstractUser):
     """Custom user model with `patronomyc` field
-    and mandatory first_name and last_name fields.
+    and mandatory first_name, last_name and email fields.
     """
 
     first_name = models.CharField(verbose_name=_('first name'), max_length=150)
     last_name = models.CharField(verbose_name=_('last name'), max_length=150)
     patronymic = models.CharField(verbose_name=_('patronomyc'), max_length=150)
+    email = models.EmailField(verbose_name=_('email address'))
 
     def get_full_name(self) -> str:
         """Return the last_name plus the first_name plus the patronymic,
         with a space in between.
         """
         return f'{self.last_name} {self.first_name} {self.patronymic}'
+
+    def clean(self) -> None:
+        try:
+            AllowedEmail.objects.get(email=self.email)
+        except AllowedEmail.DoesNotExist:
+            raise NotAllowedEmailValidationError(
+                message=_('Email is not in allowed list')
+            )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save()
--- api/src/apps/users/tests.py
+++ api/src/apps/users/tests.py
@@ -1,19 +1,38 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from .exceptions import NotAllowedEmailValidationError
+from .models import AllowedEmail
+
+
 User = get_user_model()
 
 
 class CustomUserTests(TestCase):
-    def test_create_customuser(self):
+    def test_create_allowed_customuser(self):
+        AllowedEmail.objects.create(email='example@example.com')
+
         user = User.objects.create(
             username='user',
             password='pass',
             first_name='Ivan',
             last_name='Ivanov',
-            patronymic='Ivanovich'
+            patronymic='Ivanovich',
+            email='example@example.com'
         )
 
         self.assertEqual(user.first_name, 'Ivan')
         self.assertEqual(user.last_name, 'Ivanov')
         self.assertEqual(user.patronymic, 'Ivanovich')
+        self.assertEqual(user.email, 'example@example.com')
+
+    def test_create_no_allowed_customuser(self):
+        with self.assertRaises(NotAllowedEmailValidationError):
+            User.objects.create(
+                username='user2',
+                password='pass',
+                first_name='Ivan',
+                last_name='Ivanov',
+                patronymic='Ivanovich',
+                email='example2@example.com'
+            )
