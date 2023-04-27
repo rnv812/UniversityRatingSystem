1512e191f10f64bc90bdbc9b44c95aa64279b258
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-04-27 16:25:13 +0300

    Add action for getting criterions in specific partition (#53)

--- api/src/apps/rating/views.py
+++ api/src/apps/rating/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Criterion, Indicator, RatingPartition, ValueType
@@ -13,6 +18,21 @@ class ValueTypeViewSet(ReadOnlyModelViewSet):
 class RatingPartitionViewSet(ReadOnlyModelViewSet):
     queryset = RatingPartition.objects.all().order_by('pk')
     serializer_class = RatingPartitionSerializer
+
+    @action(
+        detail=True,
+        methods=SAFE_METHODS
+    )
+    def criterions(self, request: Request, pk: str) -> Response:
+        """Get list of criterions in specifid partition."""
+
+        partition = get_object_or_404(RatingPartition, pk=pk)
+
+        criterion_pks = Criterion.objects.filter(
+            partition=partition
+        ).values_list('pk', flat=True)
+
+        return Response(criterion_pks)
 
 
 class IndicatorViewSet(ReadOnlyModelViewSet):
