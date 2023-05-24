6b837cfda79429c6c5fea64b8265a05ca0c36b88
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-05-24 17:50:52 +0300

    Add create new report modal form

--- client/src/pages/ReportsPage.jsx
+++ client/src/pages/ReportsPage.jsx
@@ -1,14 +1,62 @@
 import * as React from "react";
-import {Box} from "@mui/material"
+import {
+    Button,
+    Box,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from "@mui/material"
 import Navbar from "../components/Navbar";
 import ReportListContainer from "../components/ReportListContainer";
+import ItemSelect from '../components/ItemSelect';
 
 
 export default function ReportsPage() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Box>
-            <Navbar actions={[{name: "Создать анкету", func: null}, {name: "Выйти", func: null}]} />
+            <Navbar actions={[{name: "Создать анкету", func: handleClickOpen}, {name: "Выйти", func: null}]} />
             <ReportListContainer reports={[{id: 1}, {id: 2}, {id: 3}]}/>
+            <Dialog
+                open={open}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Создание анкеты"}</DialogTitle>
+                <DialogContent>
+                    <Box style={{display:"flex", flexDirection: "column"}}>
+                        <ItemSelect
+                            id={'report-educator'}
+                            title={"Преподаватель"}
+                            items={[{label: "Фамилия Имя Отчество", value: "TestName"}]}
+                            defaultValue={"TestName"}
+                            sx={{ minWidth: "340px", marginTop: "20px" }}
+                        />
+                        <ItemSelect
+                            id={'report-year'}
+                            title={"Год анкеты"}
+                            items={[{label: "2023", value: "2022"}]}
+                            defaultValue={"2023"}
+                            sx={{ minWidth: "340px", marginTop: "20px"}}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose}>Создать</Button>
+                <Button onClick={handleClose}>Отменить</Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 }
