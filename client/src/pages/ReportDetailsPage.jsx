0f37caf33a0277c9c8a61239d02bc695e8d10a11
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-06-11 09:06:51 +0300

    Add fullname to navbar for report details page

--- client/src/pages/ReportDetailsPage.jsx
+++ client/src/pages/ReportDetailsPage.jsx
@@ -4,9 +4,13 @@ import { Box } from "@mui/material"
 import styles from "../styles/Page.module.css"
 import ReportListNavbar from "../components/Navbar";
 import ReportFormContainer from "../components/ReportFormContainer";
+import { useSelector } from 'react-redux'
+import { selectCurrentUser } from '../features/auth/authSlice'
+
 
 export default function ReportDetailsPage() {
     const navigate = useNavigate();
+    const user = useSelector(selectCurrentUser);
     
     function backToReports() {
         navigate('/reports')
@@ -17,11 +21,9 @@ export default function ReportDetailsPage() {
         {name: "Выйти", func: null}
     ]
 
-    var fullname = "Фамилия Имя Отчество"
-
     return (
         <Box>
-            <ReportListNavbar actions={navbarActions} pageTitle={fullname}/>
+            <ReportListNavbar actions={navbarActions} pageTitle={ `${user.last_name}  ${user.first_name} ${user.patronymic}` } />
             <Box className={ `${styles.center} ${styles.pageTopStart}` }>
                 <ReportFormContainer />
             </Box>
