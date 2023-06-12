41aa5cf48df843f248357946aab0f8e7098709ce
Nikita Reznikov <nikita.reznikov.live@mail.ru>
2023-06-12 10:50:31 +0300

    Update query for fetching employee data

--- api/src/apps/integration_1c/queries.py
+++ api/src/apps/integration_1c/queries.py
@@ -5,14 +5,20 @@ import requests as rq
 
 
 EMPLOYEES_API_URL = os.getenv('EMPLOYEES_API_URL')
-EMPLOYEES_MOCK_API_URL = os.getenv('EMPLOYEES_MOCK_API_URL')
+
+_api_auth_username = os.getenv('EMPLOYEES_API_USERNAME')
+_api_auth_password = os.getenv('EMPLOYEES_API_PASSWORD')
+
+_HEADERS = {
+    'Authorization': f"Basic {_api_auth_username}:{_api_auth_password}"
+}
 
 
 def query_employee_data(employee_id: str) -> Optional[dict]:
     """Fetch information about employee from api by his employee id."""
     try:
-        url = f'{EMPLOYEES_API_URL}/endpoint/'
-        response = rq.get(url, params={'employee_id': employee_id})
+        url = f'{EMPLOYEES_API_URL}/RATING/hs/educatorData/{employee_id}/'
+        response = rq.get(url, headers=_HEADERS)
         return response.json()
     except rq.exceptions.RequestException:
         return None
